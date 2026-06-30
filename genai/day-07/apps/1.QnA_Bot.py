0de77aa3865cdf415148d57e182b9e4aad200419
nishantchaurasiya22 <25f2008456@ds.study.iitm.ac.in>
2026-06-30 16:13:42 +0530

    make a basic chatbot using langchain,steamlit and groq;s llm model for fast reply

---
+++ genai/day-07/apps/1.QnA_Bot.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_groq import ChatGroq
+import streamlit as st
+
+llm=ChatGroq(model="llama-3.3-70b-versatile")
+
+st.title("🤖 ECHO — AI-powered Q&A Assistant")
+st.markdown("A QnA chatbot using LangChain, Streamlit and Groq's llama model.")
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages=[]
+
+for message in st.session_state.messages:
+    role=message["role"]
+    content=message["content"]
+    st.chat_message(role).markdown(content)
+
+
+question=st.chat_input("Ask me anything...")
+if question:
+    st.session_state.messages.append({"role":"user","content":question})
+    st.chat_message("user").markdown(question)
+    answer=llm.invoke(question)
+    st.chat_message("ai").markdown(answer.content)
+    st.session_state.messages.append({"role":"ai","content":answer.content})
