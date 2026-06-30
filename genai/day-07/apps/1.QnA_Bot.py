b1fc68f1dc4c49d0a7bfd8914c0c313449b9ea53
nishantchaurasiya22 <25f2008456@ds.study.iitm.ac.in>
2026-06-30 18:54:39 +0530

    add memory in my chatbot

--- genai/day-07/apps/1.QnA_Bot.py
+++ genai/day-07/apps/1.QnA_Bot.py
@@ -8,7 +8,7 @@ llm=ChatGroq(model="llama-3.3-70b-versatile")
 
 st.title("🤖 ECHO — AI-powered Q&A Assistant")
 st.markdown("A QnA chatbot using LangChain, Streamlit and Groq's llama model.")
-
+st.markdown("<style>.block-container{margin-inline:1rem;}</style>", unsafe_allow_html=True)
 
 if "messages" not in st.session_state:
     st.session_state.messages=[]
@@ -16,13 +16,13 @@ if "messages" not in st.session_state:
 for message in st.session_state.messages:
     role=message["role"]
     content=message["content"]
-    st.chat_message(role).markdown(content)
+    st.chat_message(role).markdown(content) 
 
 
 question=st.chat_input("Ask me anything...")
 if question:
     st.session_state.messages.append({"role":"user","content":question})
     st.chat_message("user").markdown(question)
-    answer=llm.invoke(question)
+    answer=llm.invoke(st.session_state.messages)
     st.chat_message("ai").markdown(answer.content)
     st.session_state.messages.append({"role":"ai","content":answer.content})
